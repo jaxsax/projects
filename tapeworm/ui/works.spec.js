ef68e315bcf61632e8fab06357f690b91c489589
Sheng Wei <shengweichow@gmail.com>
2019-10-27 23:00:32 +0800

    Add basic parcel example

--- tapeworm/ui/works.spec.js
+++ tapeworm/ui/works.spec.js
@@ -1,5 +1,17 @@
+// const fs = require('fs');
+
 describe('it works?', () => {
     it('should be working', () => {
         expect(process.version).toBe('v13.0.1');
+    })
+})
+
+describe('bundling', () => {
+    it('should work', () => {
+        let written;
+
+        console.log = (m) => written = m;
+        const bundle = require('com_github_jaxsax_projects/tapeworm/ui/bundle.js');
+        expect(written).toEqual('Hello, Bob');
     })
 })
