d6e47e8569dfe28fd954654b841a978146050558
jaxsax <shengweichow@gmail.com>
2019-12-28 23:02:59 +0800

    Add python example

---
+++ python_test/lib.py
@@ -0,0 +1,2 @@
+def name_builder(first, last):
+    return f"{first} {last}"
---
+++ python_test/lib_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+import lib as l
+from lib import name_builder
+
+class TypicalUnitTest(unittest.TestCase):
+    def test_works(self):
+        self.assertEqual(1,1)
+
+    def test_name_builder(self):
+        got = name_builder('a', 'b')
+        want = 'a b'
+        self.assertEqual(want, got)
+
+    def test_name_builder_import(self):
+        got = l.name_builder('a', 'b')
+        want = 'a b'
+        self.assertEqual(want, got)
