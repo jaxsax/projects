b4e46c44cf7a68f29d925730a62be4e42da5b62d
jaxsax <shengweichow@gmail.com>
2019-12-29 16:46:46 +0800

    Formatting

--- tapeworm_bot/bot/bot.py
+++ tapeworm_bot/bot/bot.py
@@ -2,4 +2,4 @@ import lib
 
 if __name__ == "__main__":
     app = lib.create_app()
-    app.run('127.0.0.1', port=8080, debug=True)
+    app.run("127.0.0.1", port=8080, debug=True)
--- tapeworm_bot/bot/lib.py
+++ tapeworm_bot/bot/lib.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
-def create_app(env='dev'):
-    app = Flask(
-        __name__
-    )
 
-    @app.route('/')
+def create_app(env="dev"):
+    app = Flask(__name__)
+
+    @app.route("/")
     def root():
-        return {'a': 1}
+        return {"hello": 1}
 
     return app
