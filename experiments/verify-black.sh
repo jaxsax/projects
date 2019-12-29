5c1f44ec47e70d3cb2969aa0c047fdf024bf6ea0
jaxsax <shengweichow@gmail.com>
2019-12-29 14:31:25 +0800

    Add back verify-black

---
+++ experiments/verify-black.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
+if [[ -n "${TEST_WORKSPACE:-}" ]]; then # Running inside bazel
+  echo "Linting python..." >&2
+elif ! command -v bazel &> /dev/null; then
+  echo "Install bazel at https://bazel.build" >&2
+  exit 1
+else
+  (
+    set -o xtrace
+    bazel test --test_output=streamed //hack:verify-pylint
+  )
+  exit 0
+fi
+
+shopt -s extglob globstar
+
+"$DIR/black_bin" !(external)/**/*.py
