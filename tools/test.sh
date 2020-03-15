8d8d244ef899ccd449804946a53264c795986d07
Sheng Wei <shengweichow@gmail.com>
2020-03-16 00:14:09 +0800

    Add script to generate images locally

--- tools/test.sh
+++ tools/test.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+if [[ -n "${BUILD_WORKSPACE_DIRECTORY:-}" ]]; then
+    echo "You're running via bazel!" >&2
+elif ! command -v bazelisk &>/dev/null; then
+    echo "Install bazelisk at https://github.com/bazelbuild/bazelisk" >&2
+    exit 1
+else
+    (
+        set -o xtrace
+        bazelisk run //tools:test
+    )
+    exit 0
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "pwd: $PWD"
---
+++ tools/update-bot.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ -n "${BUILD_WORKSPACE_DIRECTORY:-}" ]]; then
+    echo "You're running via bazel!" >&2
+elif ! command -v bazelisk &>/dev/null; then
+    echo "Install bazelisk at https://github.com/bazelbuild/bazelisk" >&2
+    exit 1
+else
+    (
+        set -o xtrace
+        bazelisk run //tools:update-bot
+    )
+    exit 0
+fi
+
+if ! command -v docker &>/dev/null; then
+    echo "Requires docker"
+    exit 1
+fi
+
+(
+    set -o xtrace
+
+    cd "$BUILD_WORKSPACE_DIRECTORY"
+    bazelisk build //tapeworm/bot_v2/cmd/bot:docker.tar
+    docker load -i bazel-bin/tapeworm/bot_v2/cmd/bot/docker.tar
+    generatedImageID=$(docker images  bazel/tapeworm/bot_v2/cmd/bot:docker --format '{{.ID}}')
+    version="v$(date '+%Y%m%d-%H%M')"
+    docker tag "$generatedImageID" tapeworm/bot_v2:$version
+)
