7f6476f8badb5f77ba334b94016b849a473b8036
Sheng Wei <shengweichow@gmail.com>
2020-03-28 02:57:07 +0800

    Add CORS for links

--- tapeworm/botv2/web/server.go
+++ tapeworm/botv2/web/server.go
@@ -47,6 +47,7 @@ func (s *Server) handleLinks(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
 }
 
