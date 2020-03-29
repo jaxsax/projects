83e3259ee435e39a48cee057adad57a34ff04179
Sheng Wei <shengweichow@gmail.com>
2020-03-29 17:35:09 +0800

    Run tests before updating bot

--- tools/update-bot.sh
+++ tools/update-bot.sh
@@ -22,7 +22,7 @@ fi
     set -o xtrace
 
     cd "$BUILD_WORKSPACE_DIRECTORY"
-    bazelisk build //tapeworm/botv2/cmd/bot:docker.tar
+    bazelisk test //tapeworm/botv2/.. && bazelisk build //tapeworm/botv2/cmd/bot:docker.tar
     docker load -i bazel-bin/tapeworm/botv2/cmd/bot/docker.tar
     generatedImageID=$(docker images  bazel/tapeworm/botv2/cmd/bot:docker --format '{{.ID}}')
     version="v$(date '+%Y%m%d-%H%M')"
