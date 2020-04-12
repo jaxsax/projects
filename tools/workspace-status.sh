fc750bb6ddcdbbe028b3c00b787cc531811a49a2
Sheng Wei <shengweichow@gmail.com>
2020-04-12 15:01:47 +0800

    Add localhost as default docker repository

--- tools/workspace-status.sh
+++ tools/workspace-status.sh
@@ -8,6 +8,6 @@ git_commit="$(git describe --tags --always --dirty)"
 build_date="$(date -u '+%Y%m%d')"
 docker_tag="v${build_date}-${git_commit}"
 cat <<EOF
-DOCKER_REPO ${DOCKER_REPO}
+DOCKER_REPO ${DOCKER_REPO:-localhost:5000}
 DOCKER_TAG ${docker_tag}
 EOF
