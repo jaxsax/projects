7d8d4335024ae369302f8b4b3bc4e239af09addc
Sheng Wei <shengweichow@gmail.com>
2020-04-12 19:15:32 +0800

    Add update script for bot-ui

---
+++ tools/update-bot-ui.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ -n "${BUILD_WORKSPACE_DIRECTORY:-}" ]]; then
+    echo "You're running via bazel!" >&2
+elif ! command -v bazelisk &>/dev/null; then
+    echo "Install bazelisk at https://github.com/bazelbuild/bazelisk" >&2
+    exit 1
+else
+    (
+        set -o xtrace
+        bazelisk run //tools:update-bot-ui
+    )
+    exit 0
+fi
+
+if ! command -v docker &>/dev/null; then
+    echo "Requires docker"
+    exit 1
+fi
+
+(
+    set -o xtrace
+    set -e
+
+    cd "$BUILD_WORKSPACE_DIRECTORY"
+    bazelisk run //tapeworm/uiv2/nginx-static:push
+)
