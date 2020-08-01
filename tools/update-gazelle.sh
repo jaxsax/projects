2d7d24ea4dbaa1c636b8252a4accffdb60e20737
Sheng Wei <shengweichow@gmail.com>
2020-08-01 14:59:16 +0800

    Add script to run gazelle update

---
+++ tools/update-gazelle.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ -n "${BUILD_WORKSPACE_DIRECTORY:-}" ]]; then
+    echo "You're running via bazel!" >&2
+elif ! command -v bazelisk &>/dev/null; then
+    echo "Install bazelisk at https://github.com/bazelbuild/bazelisk" >&2
+    exit 1
+fi
+
+set -o xtrace
+bazelisk run //:gazelle -- update
