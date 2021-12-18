7b0d7cfcf8faf31e17c39b83710e8a8b2f18c54a
jaxsax <shengweichow@gmail.com>
2021-12-18 23:35:34 +0800

    Link item

--- tapeworm/uiv3/pages/index.tsx
+++ tapeworm/uiv3/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useQuery  } from 'react-query';
+import { useQuery } from 'react-query';
 
 function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms))
@@ -19,17 +19,45 @@ type Link = {
     created_ts: string
     created_by: number
     link: string
-    title: Map<string, any>
+    title: string
 }
 
 type Props = {
     links: Link[]
 }
 
-function LinksContainer(data: Props) {
-    console.debug(data)
+function LinkItem(l: Link) {
+    let title = l.title.replace(/(\r\n|\n|\r)/gm, " ");
+    title = l.title.replace(/(\s+)/g, " ");
+
+    let linkHostname = null;
+    try {
+        const { hostname } = new URL(l.link)
+        linkHostname = hostname
+    } catch (error) {
+    }
+
     return (
-        <div className="flex">
+        <div className="w-full">
+            <div>
+                <a href={l.link}>
+                    {title}
+                    {linkHostname !== null ?
+                        (
+                            <div className="text-gray-400">
+                                ({linkHostname})
+                            </div>
+                        ) : null}
+                </a>
+            </div>
+        </div>
+    )
+}
+
+function LinksContainer({ links }: Props) {
+    return (
+        <div className="flex flex-row flex-wrap gap-4">
+            {links.map((l) => <LinkItem key={l.id} {...l} />)}
         </div>
     )
 }
@@ -38,16 +66,16 @@ function IndexPage() {
     const { isSuccess, isLoading, data } = useLinks()
     return (
         <>
-            <div className="container mx-auto mt-12">
-                <h1 className="text-3xl text-bold">link search</h1>
+            <div className="mx-2 xl:container xl:mx-auto mt-24">
+                <h1 className="text-center text-7xl text-bold">link search</h1>
                 <form className="mt-4">
                     <input
                         type="text" name="query" placeholder="Enter search terms"
-                        className="w-1/3 px-4 py-2 border-2 border-gray-400 outline-none  focus:border-gray-400" />
+                        className="w-full px-4 py-2 border-2 border-gray-400 outline-none focus:border-gray-400" />
                 </form>
                 <div className="mt-4">
-                    {isLoading ? <h1 className="text-2xl font-bold">Loading...</h1> : null}
-                    {isSuccess ? <LinksContainer links={data} /> : null}
+                    {isLoading ? <h1 className="text-center text-2xl">Loading...</h1> : null}
+                    {isSuccess ? <LinksContainer links={data.links.slice(0, 5)} /> : null}
                 </div>
             </div>
         </>
